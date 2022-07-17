f361e10a6b8958cd1d166862519718e8abe70a0a
Paul Miller <paul@paulmillr.com>
2022-07-17 23:49:41 +0000

    Gitignore

--- test.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function bytesToHex(uint8a: Uint8Array): string;
-export declare function hexToBytes(hex: string): Uint8Array;
-export declare function numberToHex(num: number | bigint, padToBytes?: number): string;
-export declare function numberToBytes(num: number | bigint): Uint8Array;
